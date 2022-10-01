93607a9b324dde914b6efb3ebcb659fc637db86a
GyeongChan <woig9874@naver.com>
2022-10-01 14:31:15 +0000

    Add: Hooks 개념을 도입한 `메뉴 추가 기능`

---
+++ router-test/src/hooks/RouterPropFromHooks.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+
+type MenuType = {
+  length?: number;
+  menus: {
+    [key: string]: {
+      name: string;
+      price: number;
+    }
+  }
+}
+
+function Menu() {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState(0);
+
+  const [menus, setMenus] = useState<MenuType>({menus: {}});
+  
+  const onChangeName = (e: any) => {
+    setName(e.target.value)
+  }
+
+  const onChangePrice = (e: any) => {
+    setPrice(e.target.value)
+  }
+
+  function handleAddMenus<T>(e: T) {
+    setMenus({menus: {
+      [`${Math.random()}`]: {
+        name: name,
+        price: price
+      }
+    }})
+    console.log(menus);
+  }
+  
+  return (
+    <>
+      <div>
+        <input type="text" onChange={onChangeName} />
+        <input type="text" onChange={onChangePrice} />
+        <input type="button" value="등록" onClick={handleAddMenus} />
+        <h1>메뉴</h1>
+        <p></p>
+      </div>
+    </>
+  )
+}
+
+export default Menu;
