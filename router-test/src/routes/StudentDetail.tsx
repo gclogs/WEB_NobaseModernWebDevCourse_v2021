e9136ee4d885bf12949bfcef0293b5ab101c4513
GyeongChan <woig9874@naver.com>
2022-10-02 05:42:44 +0000

    Add: useLocation으로 state 가져오기

--- router-test/src/routes/StudentDetail.tsx
+++ router-test/src/routes/StudentDetail.tsx
@@ -1,8 +1,21 @@
+import { useLocation } from "react-router-dom"
+
 export default function StudentDetail() {
+  /*
+   * props가 전달이 되었는지 확인
+   */
+  const location = useLocation()
+  console.log(location)
+  const students = location.state.students;
+
   return (
     <>
-      <div>
+      <div className="/studentlist/:id">
         <h1>Student Detail</h1>
+        <div>
+          <h2>이름 : {students.name}</h2>
+          <h2>나이 : {students.age}</h2>
+        </div>
       </div>
     </>
   )
