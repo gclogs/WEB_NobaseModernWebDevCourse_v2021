c679673e3ce764ab46dda2553e87184c7d58c295
GyeongChan <woig9874@naver.com>
2022-10-02 13:43:59 +0000

    Add: page routes

--- lunchbox-assistant/src/App.tsx
+++ lunchbox-assistant/src/App.tsx
@@ -1,7 +1,22 @@
+import {BrowserRouter as Router , Route, Routes} from 'react-router-dom'
+import Join from './routes/Join'
+import Login from './routes/Login'
+import Main from './routes/Main'
+import MyOrder from './routes/MyOrder'
+import Order from './routes/Order'
+
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>}></Route>
+          <Route path="/join" element={<Join/>}></Route>
+          <Route path="/main" element={<Main/>}></Route>
+          <Route path="/myorder" element={<MyOrder/>}></Route>
+          <Route path="/order" element={<Order/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
