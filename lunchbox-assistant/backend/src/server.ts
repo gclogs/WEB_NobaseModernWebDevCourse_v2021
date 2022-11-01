cb3306973378b518139c32e1e90a9e6c039901c3
GyeongChan <woig9874@naver.com>
2022-11-01 14:27:26 +0000

    feat: Create `mongodb` connector logic

--- lunchbox-assistant/backend/src/server.ts
+++ lunchbox-assistant/backend/src/server.ts
@@ -1,10 +1,15 @@
 import Fastify from 'fastify'
 import router from './router';
+import dbConnector from './database';
 
 const fastify = Fastify({
   logger: true
 })
 
+fastify.register(dbConnector, {
+  url: 'mongodb+srv://gclogs:root123@fastify.rnzkolb.mongodb.net/?retryWrites=true&w=majority',
+  useNewUrlParser: true,
+})
 fastify.register(router)
 
 // Declare a route
