255569096c8dfe12dc361cab26f0a9dab2201d2d
GyeongChan <woig9874@naver.com>
2022-11-01 14:29:24 +0000

    add: code to check if the `mongodb` is connected

--- lunchbox-assistant/backend/src/router.ts
+++ lunchbox-assistant/backend/src/router.ts
@@ -1,6 +1,42 @@
-async function routes (fastify, options) {
-  fastify.get('/', async (request, reply) => {
+async function routes (fastify: any, options: any) {
+  const collection = fastify.mongo.db('test').collection('test');
+
+  fastify.get('/', async (request: any, reply: any) => {
     return { hello: '응애'}
+  })
+
+  fastify.get('/animals', async (request: any, reply: any) => {
+    const result = await collection.find().toArray()
+    if (result.length === 0) {
+      throw new Error('No documents found')
+    }
+    return result
+  })
+
+  fastify.get('/animals/:animal', async (request: any, reply: any) => {
+    const result = await collection.findOne({ animal: request.params.animal })
+    if (!result) {
+      throw new Error('Invalid value')
+    }
+    return result
+  })
+
+  const animalBodyJsonSchema = {
+    type: 'object',
+    required: ['animal'],
+    properties: {
+      animal: { type: 'string' },
+    },
+  }
+
+  const schema = {
+    body: animalBodyJsonSchema,
+  }
+
+  fastify.post('/animals', { schema }, async (request: any, reply: any) => {
+    // we can use the `request.body` object to get the data sent by the client
+    const result = await collection.insertOne({ animal: request.body.animal })
+    return result
   })
 }
 
