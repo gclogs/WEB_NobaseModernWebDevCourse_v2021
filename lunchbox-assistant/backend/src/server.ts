873f91410b5838826efe0a92c0264637fc5c5a7e
GyeongChan <woig9874@naver.com>
2022-11-03 14:58:53 +0000

    Issue where files in other directories cannot be imported

--- lunchbox-assistant/backend/src/server.ts
+++ lunchbox-assistant/backend/src/server.ts
@@ -1,11 +1,12 @@
 import Koa from 'koa';
+import Router from 'koa-router';
+import api from './routes/api'
 
 const app = new Koa();
+const router = new Router();
 
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
-})
+router.use('/api', api.routes())
 
 app.listen(4000, () => {
-  console.log("Listening to port 4000")
+  console.log(`Listening to port 4000`)
 })
