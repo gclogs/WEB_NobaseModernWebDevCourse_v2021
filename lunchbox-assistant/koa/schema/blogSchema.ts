7f75e652b5d99dba244f3833d0abce0c588311c8
GyeongChan <woig9874@naver.com>
2022-11-08 14:11:52 +0000

    add: example schema

---
+++ lunchbox-assistant/koa/schema/blogSchema.ts
@@ -0,0 +1,26 @@
+import { Schema } from 'mongoose';
+
+const blogSchema = new Schema({
+  title: {
+    type: String
+  },
+  author: {
+    type: String
+  },
+  body: {
+    type: String,
+  },
+  comments: [{
+    body: String,
+    date: Date
+  }],
+  date :{
+    type: Date,
+    default: Date.now
+  },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs: Number
+  }
+})
