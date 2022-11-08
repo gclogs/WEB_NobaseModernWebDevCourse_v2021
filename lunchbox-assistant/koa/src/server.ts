1ed8051a89e6c1e3a10a99318403cbdddb00cad0
GyeongChan <woig9874@naver.com>
2022-11-08 14:16:31 +0000

    Implements of `MongoDB Atlas` Interworking through `Mongoose`

--- lunchbox-assistant/koa/src/server.ts
+++ lunchbox-assistant/koa/src/server.ts
@@ -1,15 +1,20 @@
 import Koa from 'koa';
 import Router from 'koa-router';
-import api from './routes/api/index.js'
+import BodyParser from 'koa-bodyparser';
+import api from './routes/api'
+import mongoDB from './mongoose';
 
 const app = new Koa();
 const router = new Router();
+const db = new mongoDB();
 
-router.use('/api', api.routes())
+db.conn();
 
-app.use((ctx) => {
-  ctx.body = "Hello world";
-})
+app.use(BodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+router.use('/api', api.routes());
 
 app.listen(4000, () => {
   console.log(`Listening to port 4000`)
