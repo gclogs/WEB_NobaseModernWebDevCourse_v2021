e9937e0c6c0316df3c7fd6bd42a767285b543666
GyeongChan <woig9874@naver.com>
2022-11-12 14:48:36 +0000

    feat: `Check API` to logged-in user information

--- lunchbox-assistant/koa/src/routes/api/auth/auth.ctrl.ts
+++ lunchbox-assistant/koa/src/routes/api/auth/auth.ctrl.ts
@@ -38,6 +38,14 @@ const AuthCtrl = {
       ctx.throw(500, e);
     }
 
+    let token = null;
+    try {
+      token = account.generateToken();
+    } catch (e) {
+      ctx.throw(500, e);
+    }
+
+    ctx.cookies.set('access_token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 7 });
     ctx.body = account.profile;
   },
 
@@ -68,6 +76,14 @@ const AuthCtrl = {
       return;
     }
 
+    let token = null;
+    try {
+      token = await account.generateToken();
+    } catch (e) {
+      ctx.throw(500, e);
+    }
+
+    ctx.cookies.set('access_token',  { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 7 })
     ctx.body = account.profile;
   },
 
@@ -87,7 +103,22 @@ const AuthCtrl = {
   },
 
   logout(ctx) {
-    ctx.body = 'logout'
+    ctx.cookies.set('access_token', null, {
+      maxAge: 0, 
+      httpOnly: true
+    });
+    ctx.status = 204;
+  },
+
+  checkUser(ctx) {
+    const { user } = ctx.request;
+
+    if (!user) {
+      ctx.status = 403;
+      return;
+    }
+
+    ctx.body = user.profile;
   }
 }
 
