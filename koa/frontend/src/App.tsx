286d89f111e97e39b39f983a65e2c2bb1fc93f41
GyeongChan <40288646+gclogs@users.noreply.github.com>
2022-11-15 14:16:52 +0000

    fix: sub Route implementation changes from `router v6`

--- koa/frontend/src/App.tsx
+++ koa/frontend/src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/HomePage'
 import Auth from './pages/AuthPage'
 import { Provider } from 'react-redux';
+import HeaderContainer from './container/base/HeaderContainer';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = ({store}) => {
   return (
     <>
+      <HeaderContainer />
       <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/auth" element={<Auth />}/>
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/auth" element={<Auth />}>
+            <Route path="/auth/login" element={<Login />} />
+            <Route path="/auth/register" element={<Register />} />
+          </Route>
+        </Routes>
       </Provider>
     </>
   )
