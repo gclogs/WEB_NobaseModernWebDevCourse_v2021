a35952e1647b3bf3b169b58a5d5930b882d0e67a
GyeongChan <40288646+gclogs@users.noreply.github.com>
2022-11-15 14:31:28 +0000

    feat: implements link the redux state

---
+++ koa/frontend/src/container/base/HeaderContainer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/base/Header';
+import LoginButton from '../../components/base/LoginButton'
+
+const HeaderContainer = () => {
+  return (
+    <>
+      <Header>
+        <LoginButton />
+      </Header>
+    </>
+  )
+}
+
+export default HeaderContainer;
